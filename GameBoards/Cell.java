a24954346a918f505f62836461fac90ea7e551b2
Harish Kajur <hkajur93@gmail.com>
2015-02-07 20:32:22 -0500

    Finish the commenting of Cell source

--- GameBoards/Cell.java
+++ GameBoards/Cell.java
@@ -8,7 +8,7 @@ package GameBoards;
  * @version         %I%, %G%
  * @since           1.0
  */
-class Cell {
+class Cell implements Cloneable {
 
     public static final String ALIVE_CELL = "X";
     public static final String DEAD_CELL = "_";
@@ -28,7 +28,7 @@ class Cell {
     /**
      * Constructs an Cell Object with the alive and count of live neighbors
      *
-     * @param       alive
+     * @param      alive boolean setting the status of instance variable alive variable
      */
     public Cell(boolean alive){
         this(alive, MIN);
@@ -37,17 +37,19 @@ class Cell {
     /**
      * Constructs an Cell Object with the alive and count of live neighbors
      *
-     * @param       alive
-     * @param       liveneighbors
+     * @param       alive boolean setting the status of instance variable variable
+     * @param       liveneighbors byte setting the value of instance variable liveNeighbors
      */
     public Cell(boolean alive, byte liveNeighbors){
         this.alive = alive;
         this.liveNeighbors = liveNeighbors;
     }
+
     /**
      * Returns an boolean indicting if the cell is alive or not
      *
-     * @return
+     * @return      true if the status of alive is true
+     *              otherwise it will return false
      */
     public boolean isAlive(){
         return alive;
@@ -67,7 +69,7 @@ class Cell {
      *
      *  @return     liveneighbors an integer which keeps track of live cells
      */
-    public int getLiveNeighborsCount(){
+    public int getLiveNeighbors(){
         return liveNeighbors;
     }
 
@@ -81,12 +83,42 @@ class Cell {
     }
 
     /**
-     * Checks if this object is equal to the other object
+     * Returns an object which is the clone of this Cell object
+     *
+     * First it stores the clone of its super class into dummy reference variable
+     * Then it copies the instance fields of this object into dummy reference variable
+     * Afterwards, it will return the dummy reference variable
+     *
+     * @return      Object which will be a clone of this object
+     * @throws      Clonenotsupportedexception if the super class doesn't support it
+     */
+    public Object clone() throws CloneNotSupportedException {
+        Cell cloned = (Cell)super.clone();
+
+        cloned.alive = this.alive;
+        cloned.liveNeighbors = this.liveNeighbors;
+
+        return cloned;
+    }
+
+
+    /**
+     * Returns true if this object equals to the other object
+     * Otherwise it will return false
+     *
      * First checks if the object in the parameter is null
      * If the object is null, it will return false
+     * Then it checks if the obj paramter is referencing the same object
+     * as the this reference, if it is then it will return true
+     * Because if they are referencing the same object then
+     * there is no need to do the deep equals test
+     * <p>
      * Then it checks if the object is an instance of Cell
-     * If it isn't, then it can't possibly be equal to the
-     * current cell, so it will return false
+     * If it isn't, then it can't possibly be equal to
+     * this cell, so it will return false
+     * Then it will compare if they are referencing the same object
+     * If they are, then it will return true
+     * There is no need to do the deep equals anymore
      * <p>
      * If the live neighbors of the new object are not equal
      * to the other object, it will return false
@@ -103,6 +135,9 @@ class Cell {
 
         if(obj == null)
             return false;
+
+        if(this == obj)
+            return true;
 
         if(!(obj instanceof Cell))
             return false;
@@ -142,4 +177,5 @@ class Cell {
             return ALIVE_CELL;
         return DEAD_CELL;
     }
+
 }
